3135cca0cd6e8caf5adf9947d74917860cec36b0
Emmanuel Pastor <ommanu13@hotmail.fr>
2021-12-03 13:50:41 +0100

    [ADD] Add ListAvailableNetworksUseCase

---
+++ src/main/java/com/simplesmartapps/chatsystem/domain/ListAvailableNetworksUseCase.java
@@ -0,0 +1,20 @@
+package com.simplesmartapps.chatsystem.domain;
+
+import com.simplesmartapps.chatsystem.data.remote.NetworkController;
+import com.simplesmartapps.chatsystem.data.remote.NetworkListingException;
+import javafx.util.Pair;
+
+import java.net.InetAddress;
+import java.util.List;
+
+public class ListAvailableNetworksUseCase {
+    private final NetworkController mNetworkController;
+
+    public ListAvailableNetworksUseCase(NetworkController netWorkController) {
+        this.mNetworkController = netWorkController;
+    }
+
+    public List<Pair<InetAddress, String>> execute() throws NetworkListingException {
+        return mNetworkController.listAvailableNetworks();
+    }
+}

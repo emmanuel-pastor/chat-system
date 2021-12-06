cc12cacd18df04e5ce083865e25841d478a79673
Dizzix463 <romain.sixt@hotmail.com>
2021-12-06 16:20:43 +0100

    [ADD] Add NavigationUtil

--- src/main/java/com/simplesmartapps/chatsystem/ChatSystemApplication.java
+++ src/main/java/com/simplesmartapps/chatsystem/ChatSystemApplication.java
@@ -11,10 +11,12 @@ import java.io.IOException;
 
 public class ChatSystemApplication extends Application {
     public static Injector injector;
+    public static Stage appStage;
 
     @Override
     public void start(Stage stage) throws IOException {
         injector = Guice.createInjector(new CoreModule());
+        appStage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("presentation/network_listing/network_listing_page-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 840, 480);
         stage.setTitle("Chat System");
---
+++ src/main/java/com/simplesmartapps/chatsystem/presentation/util/NavigationUtil.java
@@ -0,0 +1,46 @@
+package com.simplesmartapps.chatsystem.presentation.util;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
+
+import static com.simplesmartapps.chatsystem.ChatSystemApplication.appStage;
+
+public class NavigationUtil {
+
+    public static <T> void navigateTo(Class<T> pageClass) {
+        navigate(pageClass, null);
+    }
+
+    public static <T> void navigateTo(Class<T> pageClass, Map<String, Object> args) {
+        navigate(pageClass, args);
+    }
+
+    private static <T> void navigate(Class<T> pageClass, Map<String, Object> args) {
+        try {
+            appStage.setUserData(args);
+
+            String className = pageClass.getSimpleName();
+            String classNameSnakeCase = camelToSnakeCase(className);
+            Parent root = FXMLLoader.load(Objects.requireNonNull(pageClass.getResource(classNameSnakeCase + "-view.fxml")));
+
+            double width = appStage.getWidth();
+            double height = appStage.getHeight();
+            Scene scene = new Scene(root, width, height);
+
+            appStage.setScene(scene);
+            appStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String camelToSnakeCase(String camelStr) {
+        String ret = camelStr.replaceAll("([A-Z]+)([A-Z][a-z])", "$1_$2").replaceAll("([a-z])([A-Z])", "$1_$2");
+        return ret.toLowerCase();
+    }
+}

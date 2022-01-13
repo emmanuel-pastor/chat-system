77ff9f7818e7435327ee03dd54577f095cb854b6
Emmanuel Pastor <ommanu13@hotmail.fr>
2022-01-13 10:26:12 +0100

    [MODIFY] Use ObservableSet in RuntimeDataStore for the connected users list

--- src/main/java/com/simplesmartapps/chatsystem/data/local/RuntimeDataStore.java
+++ src/main/java/com/simplesmartapps/chatsystem/data/local/RuntimeDataStore.java
@@ -1,6 +1,7 @@
 package com.simplesmartapps.chatsystem.data.local;
 
 import com.simplesmartapps.chatsystem.data.local.model.User;
+import javafx.collections.ObservableSet;
 
 import java.util.Set;
 
@@ -8,7 +9,8 @@ public interface RuntimeDataStore {
     String readUsername();
     void writeUsername(String newUsername);
 
-    Set<User> readUsersSet();
+    ObservableSet<User> readUsersSet();
+
     void addAllUsers(Set<User> newSet);
     void removeUser(String macAddress);
 }

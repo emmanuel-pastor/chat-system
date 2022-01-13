6766697c51024828c04f32ff235a6004380a6c1a
Emmanuel Pastor <ommanu13@hotmail.fr>
2022-01-13 18:33:10 +0100

    [BUGFIX]  The list view was not reflecting the state of the list at MessagingPage launch

--- src/main/java/com/simplesmartapps/chatsystem/presentation/messaging/MessagingPage.java
+++ src/main/java/com/simplesmartapps/chatsystem/presentation/messaging/MessagingPage.java
@@ -3,6 +3,7 @@ package com.simplesmartapps.chatsystem.presentation.messaging;
 import com.simplesmartapps.chatsystem.ChatSystemApplication;
 import com.simplesmartapps.chatsystem.data.local.model.User;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableSet;
 import javafx.collections.SetChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -24,8 +25,13 @@ public class MessagingPage implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        updateListView(mViewModel.mUsersSet);
         mViewModel.mUsersSet.addListener((SetChangeListener<? super User>) change -> {
-            usersListView.setItems(FXCollections.observableList(new ArrayList<>(change.getSet())));
+            updateListView(change.getSet());
         });
+    }
+
+    private void updateListView(ObservableSet<? extends User> usersSet) {
+        usersListView.setItems(FXCollections.observableList(new ArrayList<>(usersSet)));
     }
 }

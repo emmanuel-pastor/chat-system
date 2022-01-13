de926282fe2088af360a671b9a289244232921f4
Emmanuel Pastor <ommanu13@hotmail.fr>
2022-01-13 20:04:54 +0100

    [ADD] Add empty state for users list view

--- src/main/java/com/simplesmartapps/chatsystem/presentation/messaging/MessagingPage.java
+++ src/main/java/com/simplesmartapps/chatsystem/presentation/messaging/MessagingPage.java
@@ -7,8 +7,15 @@ import javafx.collections.ObservableSet;
 import javafx.collections.SetChangeListener;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -30,11 +37,28 @@ public class MessagingPage implements Initializable {
         mViewModel.mUsersSet.addListener((SetChangeListener<? super User>) change -> {
             updateListView(change.getSet());
         });
+        usersListView.setPlaceholder(emptyListPlaceholder());
         usersListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         usersListView.setCellFactory(listView -> new UsersListCell());
     }
 
     private void updateListView(ObservableSet<? extends User> usersSet) {
         usersListView.setItems(FXCollections.observableList(new ArrayList<>(usersSet)));
+    }
+
+    private VBox emptyListPlaceholder() {
+        URL imagePath = this.getClass().getResource("images/alone.png");
+        assert imagePath != null;
+        Image image = new Image(imagePath.toString());
+        ImageView imageView = new ImageView(image);
+
+        Text text = new Text("You are the only one connected right now");
+        text.setFont(new Font("System", 19));
+
+        VBox vbox = new VBox(imageView, text);
+        vbox.setSpacing(40);
+        vbox.setPadding(new Insets(0, 20, 0, 20));
+        vbox.setAlignment(Pos.CENTER);
+        return vbox;
     }
 }

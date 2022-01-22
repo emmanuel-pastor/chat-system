5e777ebbe0d0e3baa2dfb0456dfdbd7defbb851b
Emmanuel Pastor <ommanu13@hotmail.fr>
2022-01-22 16:30:42 +0100

    [ADD] Add test for the insert method in MessageDao

--- src/test/java/com/simplesmartapps/chatsystem/CoreTestDIModule.java
+++ src/test/java/com/simplesmartapps/chatsystem/CoreTestDIModule.java
@@ -1,12 +1,28 @@
 package com.simplesmartapps.chatsystem;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.simplesmartapps.chatsystem.data.local.DatabaseController;
+import com.simplesmartapps.chatsystem.data.local.dao.MessageDao;
 import com.simplesmartapps.chatsystem.data.remote.NetworkController;
 import com.simplesmartapps.chatsystem.data.remote.NetworkControllerFake;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class CoreTestDIModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(NetworkController.class).toInstance(NetworkControllerFake.getInstance());
+        bind(DatabaseController.class).in(Singleton.class);
+        bind(MessageDao.class).in(Singleton.class);
+    }
+
+    @Provides
+    @Singleton
+    Connection provideDbConnection() throws SQLException {
+        return DriverManager.getConnection("jdbc:sqlite::memory:");
     }
 }
